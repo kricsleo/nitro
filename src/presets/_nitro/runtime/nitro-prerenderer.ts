25712c4f40f10be9f56fe8f84626df20db5b73c9
kricsleo <kricsleo@163.com>
2025-05-06 15:09:24 +0800

    refactor: show prerender error defails

--- src/presets/_nitro/runtime/nitro-prerenderer.ts
+++ src/presets/_nitro/runtime/nitro-prerenderer.ts
@@ -1,8 +1,25 @@
 import "#nitro-internal-pollyfills";
+import { getRequestHeader, getRequestURL, isEvent } from "h3";
+import { consola } from "consola";
 import { useNitroApp } from "nitropack/runtime";
 import { trapUnhandledNodeErrors } from "nitropack/runtime/internal";
 
 const nitroApp = useNitroApp();
+
+nitroApp.hooks.hook("error", (error, context) => {
+  if (
+    isEvent(context.event) &&
+    getRequestHeader(context.event, "x-nitro-prerender")
+  ) {
+    const url = getRequestURL(context.event).href;
+    consola.error(
+      `[prerender errror]`,
+      `[${context.event.method}]`,
+      `[${url}]`,
+      error
+    );
+  }
+});
 
 export const localFetch = nitroApp.localFetch;
 export const closePrerenderer = () => nitroApp.hooks.callHook("close");
